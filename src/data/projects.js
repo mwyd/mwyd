98051032260f515dc701112c19c186ca2d462e18
mwyd <wydrzanin@gmail.com>
2022-04-02 16:27:56 +0200

    Fill data placeholders

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,56 +1,53 @@
 const projects = [
   {
     name: 'Waxpeer bot',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mattis.',
+    description: 'Waxpeer market assistant. Offers scanner, automatic purchases and bidding.',
     tags: ['Chrome extension', 'Vue', 'Tailwind'],
     links: [
-      { title: 'github', url: 'https://google.com' },
-      { title: 'chrome web store', url: 'https://google.com' }
+      { title: 'Github', url: 'https://github.com/mwyd/wax-bot' },
+      { title: 'Chrome web store', url: 'https://chrome.google.com/webstore/detail/wax-bot/bflfdlbidibjafpgigbmfnikagdelkdd' }
     ]
   },
   {
     name: 'Shadowpay bot',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mattis.',
+    description: 'Shadowpay market assistant. Offers scanner, automatic purchases and bidding.',
     tags: ['Chrome extension', 'Vue', 'Vuex', 'BEM'],
     links: [
-      { title: 'github', url: 'https://google.com' },
-      { title: 'chrome web store', url: 'https://google.com' }
+      { title: 'Github', url: 'https://github.com/mwyd/sp-bot' },
+      { title: 'Chrome web store', url: 'https://chrome.google.com/webstore/detail/sp-bot/mfinpmohhocnehghkjkkaeibpggddcef' }
     ]
   },
   {
     name: 'Chess app',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mattis.',
+    description: 'Lichess games observer, simple analyzer and data visualization.',
     tags: ['Web application', 'React', 'React Router', 'BEM', 'Express.js', 'Sequelize', 'Docker', 'Maria DB', 'REST'],
     links: [
-      { title: 'github', url: 'https://google.com' },
-      { title: 'chrome web store', url: 'https://google.com' }
+      { title: 'Github', url: 'https://github.com/mwyd/chess-app-backend' }
     ]
   },
   {
     name: 'Conduit',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mattis.',
+    description: 'Steam and Shadowpay markets observer. Powers Shadowpay and Waxpeer extensions.',
     tags: ['Web application', 'Vue', 'Vue Router', 'Vuex', 'BEM', 'Laravel', 'Maria DB', 'REST'],
     links: [
-      { title: 'github', url: 'https://google.com' },
-      { title: 'chrome web store', url: 'https://google.com' }
+      { title: 'Github', url: 'https://github.com/mwyd/conduit' },
+      { title: 'Page', url: 'https://conduit.ddns.net' }
     ]
   },
   {
     name: 'pSockets',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mattis.',
+    description: 'WebSocket client implementation for PHP.',
     tags: ['Library', 'PHP', 'WebSocket'],
     links: [
-      { title: 'github', url: 'https://google.com' },
-      { title: 'chrome web store', url: 'https://google.com' }
+      { title: 'Github', url: 'https://github.com/mwyd/pSockets-client' }
     ]
   },
   {
     name: 'm.wyd',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mattis.',
+    description: 'Portfolio page.',
     tags: ['Web static page', 'React', 'React Router', 'Tailwind'],
     links: [
-      { title: 'github', url: 'https://google.com' },
-      { title: 'chrome web store', url: 'https://google.com' }
+      { title: 'Github', url: 'https://github.com/mwyd/mwyd' }
     ]
   }
 ]
